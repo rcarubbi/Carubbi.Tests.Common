8a31826c9b03dcb8151387fed2c03c3c50ea5158
Raphael Carubbi <rcarubbi@gmail.com>
2024-06-23 04:50:51 +0100

    Add project files.

---
+++ Carubbi.Tests.Common/TestBase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Carubbi.Tests.Common;
+
+public abstract class TestBase<SUT> where SUT : class
+{
+    protected TestBase()
+    {
+        LoggerFactory = new TestLoggerFactory(LoggerMock);
+        Sut = ArrangeSut();
+
+    }
+
+    protected SUT Sut { get; }
+
+    protected abstract SUT ArrangeSut();
+
+    protected TestLoggerFactory LoggerFactory { get; }
+
+    protected Mock<ILogger> LoggerMock { get; } = new();
+
+}
---
+++ Carubbi.Tests.Common/TestLoggerFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Carubbi.Tests.Common;
+
+public class TestLoggerFactory(Mock<ILogger> loggerMock) : ILoggerFactory
+{
+    public void AddProvider(ILoggerProvider provider)
+    {
+       
+    }
+
+    public ILogger CreateLogger<T>()
+    {
+        return loggerMock.Object;
+    }
+
+    public ILogger CreateLogger(string categoryName)
+    {
+        return loggerMock.Object;
+    }
+
+    public void Dispose()
+    {
+        
+    }
+}
